bc1c8f4a289f409b769add7ee0c44233d49e83ae
Richard Havalda <havalda.r94@gmail.com>
2021-10-19 19:59:10 +0200

    feat: fetch pokemons from api

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import PokemonListItem from './PokemonListItem/PokemonListItem';
+import axios from 'axios';
 
 class PokemonList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pokemons: [{ name: 'first' }, { name: 'second' }, { name: 'third' }],
+      pokemons: [],
     };
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon')
+      .then((response) => this.setState({ pokemons: response.data.results }));
+    //.then((response) => console.log(response.data.results));
   }
 
   render() {
     return (
       <div>
-        {this.state.pokemons.map((pokemon) => (
-          <PokemonListItem name={pokemon.name} />
+        {this.state.pokemons.map((pokemon, index) => (
+          <PokemonListItem name={pokemon.name} key={index} />
         ))}
       </div>
     );
