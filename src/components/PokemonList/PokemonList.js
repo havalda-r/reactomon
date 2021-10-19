caae5029c13ebd930ec16d4ec3c08a4a4254f7e2
Richard Havalda <havalda.r94@gmail.com>
2021-10-19 19:43:13 +0200

    feat: initialize pokemonlist with dummy data

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 
 class PokemonList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemons: [{ name: 'first' }, { name: 'second' }, { name: 'third' }],
+    };
+  }
+
   render() {
-    return <h1>PokemonList component</h1>;
+    return (
+      <div>
+        {this.state.pokemons.map((pokemon) => (
+          <h1>{pokemon.name}</h1>
+        ))}
+      </div>
+    );
   }
 }
 
