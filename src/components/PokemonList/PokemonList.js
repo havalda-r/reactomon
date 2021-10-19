76ea556755124654d213f267b02c57018e0805f6
Richard Havalda <havalda.r94@gmail.com>
2021-10-19 20:10:45 +0200

    feat: fetch pokemon detailed data

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -14,14 +14,13 @@ class PokemonList extends Component {
     axios
       .get('https://pokeapi.co/api/v2/pokemon')
       .then((response) => this.setState({ pokemons: response.data.results }));
-    //.then((response) => console.log(response.data.results));
   }
 
   render() {
     return (
       <div>
         {this.state.pokemons.map((pokemon, index) => (
-          <PokemonListItem name={pokemon.name} key={index} />
+          <PokemonListItem url={pokemon.url} name={pokemon.name} key={index} />
         ))}
       </div>
     );
--- src/components/PokemonList/PokemonListItem/PokemonListItem.js
+++ src/components/PokemonList/PokemonListItem/PokemonListItem.js
@@ -1,6 +1,18 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
 class PokemonListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemon: null,
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(this.props.url)
+      .then((response) => this.setState({ pokemon: response.data }));
+  }
   render() {
     return <h1>{this.props.name}</h1>;
   }
