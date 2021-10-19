73eeafc33e5ab22edd5cc884d1494eea0d4cde9e
Richard Havalda <havalda.r94@gmail.com>
2021-10-19 18:54:16 +0200

    feat: set up routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <Layout>
-          <main>MAIN CONTENT</main>
-        </Layout>
-      </div>
+      <BrowserRouter>
+        <div className='App'>
+          <Layout>
+            <main>MAIN CONTENT</main>
+          </Layout>
+        </div>
+      </BrowserRouter>
     );
   }
 }
