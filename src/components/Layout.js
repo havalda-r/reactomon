4dda44eb6772bad2158d362d49fbd242bf9c7699
Richard Havalda <havalda.r94@gmail.com>
2021-10-19 18:26:27 +0200

    feat: add layout component

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const layout = () => {
+  return (
+    <div>
+      <header>HEADER</header>
+      <main>MAIN</main>
+      <footer>FOOTER</footer>
+    </div>
+  );
+};
+
+export default layout;
