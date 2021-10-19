8abb031c79c277a1e3298040c3c532d4b9be907e
Richard Havalda <havalda.r94@gmail.com>
2021-10-19 21:58:50 +0200

    feat: fetch detailed data

--- src/components/PokemonDetail/PokemonDetail.js
+++ src/components/PokemonDetail/PokemonDetail.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class PokemonDetail extends Component {
+  state = { pokemon: null };
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://pokeapi.co/api/v2/pokemon/${this.props.match.params.pokemonId}`
+      )
+      .then((response) => this.setState({ pokemon: response.data }));
+  }
   render() {
-    return <h1>Pokemon details</h1>;
+    return (
+      <div>
+        <h1>Pokemon details</h1>
+        {this.state.pokemon ? (
+          <h1>{this.state.pokemon.name}</h1>
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </div>
+    );
   }
 }
 
