1e8586307aed3ff90182f58b825fb7824d741cb6
Richard Havalda <havalda.r94@gmail.com>
2021-10-23 21:49:58 +0200

    feat: add header style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,12 +6,14 @@ const header = () => {
   return (
     <header
       style={{
+        padding: '15px',
         position: 'sticky',
         top: '0',
-        background: 'black',
+        background: '#d10000',
+        borderBottom: '3px solid black',
       }}
     >
-      <img src={logo} alt='logo' style={{ width: '20%' }} />
+      <img src={logo} alt='logo' style={{ width: '15%' }} />
       <Navbar />
     </header>
   );
