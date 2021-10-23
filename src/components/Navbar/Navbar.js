14964ff422594f31faf48cfdf3aac227123aa1f1
Richard Havalda <havalda.r94@gmail.com>
2021-10-23 21:50:26 +0200

    feat: add navbar style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,11 +4,39 @@ import { Link } from 'react-router-dom';
 const navbar = () => {
   return (
     <div>
-      <button>
+      <button
+        style={{
+          //backgroundColor: '#f44336',
+          background: '#d10000',
+          color: '2px solid white',
+          padding: '16px 32px',
+          textAlign: 'center',
+          textDecoration: 'none',
+          display: 'inline-block',
+          fontSize: '16px',
+          margin: '14px 12px 0px 12px',
+          cursor: 'pointer',
+          width: '150px',
+        }}
+      >
         <Link to='/pokemons'>Pokémons</Link>
       </button>
 
-      <button>
+      <button
+        style={{
+          //backgroundColor: '#f44336',
+          background: '#d10000',
+          color: '2px solid white',
+          padding: '16px 32px',
+          textAlign: 'center',
+          textDecoration: 'none',
+          display: 'inline-block',
+          fontSize: '16px',
+          margin: '14px 12px 0px 12px',
+          cursor: 'pointer',
+          width: '150px',
+        }}
+      >
         <Link to='/types'>Types</Link>
       </button>
     </div>
