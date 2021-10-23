8fb33ede4b3e8444fb7e973ee1ce268cf3e9312d
Richard Havalda <havalda.r94@gmail.com>
2021-10-23 17:53:42 +0200

    feat: initialize sticky footer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,27 @@ import Header from '../Header/Header';
 
 const layout = (props) => {
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
       <Header />
       <main>{props.children}</main>
-      <footer>FOOTER</footer>
+      <footer
+        style={{
+          position: 'sticky',
+          marginTop: 'auto',
+          bottom: '0',
+          padding: '2em',
+          width: 'auto',
+          background: 'green',
+        }}
+      >
+        FOOTER
+      </footer>
     </div>
   );
 };
