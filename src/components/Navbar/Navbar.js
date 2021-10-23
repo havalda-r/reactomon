4a1b87ee04e065ce3227430e275cba8abe68cab4
Richard Havalda <havalda.r94@gmail.com>
2021-10-23 21:55:31 +0200

    fix: button border

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,41 +4,47 @@ import { Link } from 'react-router-dom';
 const navbar = () => {
   return (
     <div>
-      <button
-        style={{
-          //backgroundColor: '#f44336',
-          background: '#d10000',
-          color: '2px solid white',
-          padding: '16px 32px',
-          textAlign: 'center',
-          textDecoration: 'none',
-          display: 'inline-block',
-          fontSize: '16px',
-          margin: '14px 12px 0px 12px',
-          cursor: 'pointer',
-          width: '150px',
-        }}
-      >
-        <Link to='/pokemons'>Pokémons</Link>
-      </button>
+      <Link to='/pokemons'>
+        <button
+          style={{
+            //backgroundColor: '#f44336',
+            background: '#d10000',
+            borderColor: '2px solid white',
+            color: 'white',
+            padding: '16px 32px',
+            textAlign: 'center',
+            textDecoration: 'none',
+            display: 'inline-block',
+            fontSize: '16px',
+            margin: '14px 12px 0px 12px',
+            cursor: 'pointer',
+            width: '150px',
+          }}
+        >
+          Pokémons
+        </button>
+      </Link>
 
-      <button
-        style={{
-          //backgroundColor: '#f44336',
-          background: '#d10000',
-          color: '2px solid white',
-          padding: '16px 32px',
-          textAlign: 'center',
-          textDecoration: 'none',
-          display: 'inline-block',
-          fontSize: '16px',
-          margin: '14px 12px 0px 12px',
-          cursor: 'pointer',
-          width: '150px',
-        }}
-      >
-        <Link to='/types'>Types</Link>
-      </button>
+      <Link to='/types'>
+        <button
+          style={{
+            //backgroundColor: '#f44336',
+            background: '#d10000',
+            borderColor: '2px solid white',
+            color: 'white',
+            padding: '16px 32px',
+            textAlign: 'center',
+            textDecoration: 'none',
+            display: 'inline-block',
+            fontSize: '16px',
+            margin: '14px 12px 0px 12px',
+            cursor: 'pointer',
+            width: '150px',
+          }}
+        >
+          Types
+        </button>
+      </Link>
     </div>
   );
 };
