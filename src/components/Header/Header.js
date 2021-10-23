4ce6c86124793a1d8904341e3fac36bee93c8c10
Richard Havalda <havalda.r94@gmail.com>
2021-10-23 21:55:15 +0200

    feat: add link to homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const header = () => {
   return (
@@ -13,7 +14,13 @@ const header = () => {
         borderBottom: '3px solid black',
       }}
     >
-      <img src={logo} alt='logo' style={{ width: '15%' }} />
+      <Link to='/'>
+        <img
+          src={logo}
+          alt='logo'
+          style={{ width: '15%', cursor: 'pointer' }}
+        />
+      </Link>
       <Navbar />
     </header>
   );
